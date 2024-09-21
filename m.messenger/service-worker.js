6bbec8b3def78cab4e709d836dd674083b3c4f3e
flxn05 <163238790+flxn05@users.noreply.github.com>
2024-09-21 18:55:24 +0200

    Update service-worker.js --m-whatsapp.css targeted

--- m.messenger/service-worker.js
+++ m.messenger/service-worker.js
@@ -4,7 +4,7 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         '/index.html',
-        '/w.whatsapp.css',
+        '/m.whatsapp.css',
         '/client.js',
         '/website2.js',
         '/thumbnail.png',
