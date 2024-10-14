a0c9c4b6f4bd9fcc3bb55b67d6d461eeb5b1c9f0
MulitmeterManiac <168659953+MulitmeterManiac@users.noreply.github.com>
2024-10-14 17:36:59 +0000

    Update website2.js

--- gigachat/website2.js
+++ gigachat/website2.js
@@ -194,7 +194,7 @@ function toggle_menu(){
 }
 
 function logout(){
-    document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
     window.location.href = login_page;
 }
 
