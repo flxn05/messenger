b5c35a09d2268ff228f55cbb327ce0d215e0124e
MultimeterManiac <joasdo26@gmail.com>
2024-10-27 16:28:22 +0100

    js

--- m.gigachat/website2.js
+++ m.gigachat/website2.js
@@ -1,7 +1,7 @@
 //consts
 
 const login_page = "https://gigachat.ddns.net/login"
-const logged_out_page = "https://gigachat.ddns.net/logged_out"
+const logged_out_page = "https://gigachat.ddns.net/m.logged_out"
 
 
 
