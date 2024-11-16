31d99e60bcd1a76091de4f8ec689d5d9b7dfbdde
gerbil008 <163928546+gerbil008@users.noreply.github.com>
2024-11-16 18:12:09 +0100

    Update client.js

--- gigachat/client.js
+++ gigachat/client.js
@@ -2,48 +2,65 @@
 let socket = new WebSocket("wss://gigachat.ddns.net:12356");
 let response = "";
 let wopened = false;
+let encrypt;
+let decrypt;
+
+Module.onRuntimeInitialized = () => {
+                encrypt = Module.cwrap('encrypt', 'string', ['string']);
+                decrypt = Module.cwrap('decrypt', 'string', ['string']);};
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+function csend(msg){
+    //socket.send(encrypt(msg));
+    socket.send(msg);
+}
+
 async function get_clients(){
     if(wopened){
-        socket.send("c");
+        csend("c");
     while(response == ""){
         await sleep(10);
     }
     let rresponse = response;
     response = "";
-    return rresponse;}
+    return decrypt(rresponse);}
 }
 
 async function send_json(json, filename){
     if(wopened){
-    socket.send("r"+filename+","+json);} 
+    csend("r"+filename+","+json);} 
 }
 
 async function get_updated(json, filename){
     if(wopened){
-    socket.send("j" + filename+","+json);
+    csend("j" + filename+","+json);
     while(response == ""){
         await sleep(10);
     }
     let rresponse = response;
     response = "";
-    return rresponse;}
+    return decrypt(rresponse);}
 
 }
 
 async function get_json(filename){
     if(wopened){
-        socket.send("s"+filename);
+        csend("s"+filename);
         while(response == ""){
             await sleep(10);
         }
         let rresponse = response;
         response = "";
-        return rresponse;}
+        return decrypt(rresponse);}
+}
+
+async function make_group_chat(groupname, users){
+    if(wopened){
+        csend("m"+groupname+"%"+users);
+    }
 }
 
 socket.onopen = async function(e) {
@@ -67,4 +84,3 @@ socket.onerror = function(error) {
     wopened = false;
   alert(`[error] please refresh page or contact developers`);
 };
-
